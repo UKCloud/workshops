c360dd52c3c2aae1ee7fe8d2cf20c7701b128c2a
Rob Coward <rob@jive-videos.net>
2016-02-15 14:58:24 +0000

    Added part 1 of the terraform & chef workshop content

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# workshops
-Content used in workshops run by Skyscape Cloud Services
+# Skyscape Workshops
+Content used in workshops run by Skyscape Cloud Services.
+
+#### Terraform_and_Chef
+This folder contains the Terraform configuration and Chef cookbooks used in the "Using Terraform & Chef to automate your Skyscape Infrastructure" workshop run by Skyscape.
+
+License and Authors
+-------------------
+Authors:
+  * Rob Coward (rcoward@skyscapecloud.com)
+
+Copyright 2016 Skyscape Cloud Services
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ Terraform_and_Chef/Part1/main.tf
@@ -0,0 +1,80 @@
+variable "vcd_org"        {}
+variable "vcd_userid"     {}
+variable "vcd_pass"       {}
+variable "catalog"        { default = "DevOps" }
+variable "vapp_template"  { default = "centos71" }
+variable "edge_gateway"   { default = "Edge Gateway Name" }
+variable "jumpbox_ext_ip" {}
+variable "mgt_net_cidr"   { default = "10.10.0.0/24" }
+variable "jumpbox_int_ip" { default = "10.10.0.100" }
+ 
+# Configure the VMware vCloud Director Provider
+provider "vcd" {
+    user            = "${var.vcd_userid}"
+    org             = "${var.vcd_org}"
+    password        = "${var.vcd_pass}"
+    url             = "https://api.vcd.portal.skyscapecloud.com/api"
+}
+
+# Create our networks
+resource "vcd_network" "mgt_net" {
+    name         = "Management Network"
+    edge_gateway = "${var.edge_gateway}"
+    gateway      = "${cidrhost(var.mgt_net_cidr, 1)}"
+
+    static_ip_pool {
+        start_address = "${cidrhost(var.mgt_net_cidr, 10)}"
+        end_address   = "${cidrhost(var.mgt_net_cidr, 200)}"
+    }
+}
+
+# Jumpbox VM on the Management Network
+resource "vcd_vapp" "jumpbox" {
+    name          = "jump01"
+    catalog_name  = "${var.catalog}"
+    template_name = "${var.vapp_template}"
+    memory        = 512
+    cpus          = 1
+    network_name  = "${vcd_network.mgt_net.name}"
+    ip            = "${var.jumpbox_int_ip}"
+}
+
+# Inbound SSH to the Jumpbox server
+resource "vcd_dnat" "jumpbox-ssh" {
+    edge_gateway  = "${var.edge_gateway}"
+    external_ip   = "${var.jumpbox_ext_ip}"
+    port          = 22
+    internal_ip   = "${var.jumpbox_int_ip}"
+}
+
+# SNAT Outbound traffic
+resource "vcd_snat" "mgt-outbound" {
+    edge_gateway  = "${var.edge_gateway}"
+    external_ip   = "${var.jumpbox_ext_ip}"
+    internal_ip   = "${var.mgt_net_cidr}"
+}
+
+resource "vcd_firewall_rules" "website-fw" {
+    edge_gateway   = "${var.edge_gateway}"
+    default_action = "drop"
+
+    rule {
+        description      = "allow-jumpbox-ssh"
+        policy           = "allow"
+        protocol         = "tcp"
+        destination_port = "22"
+        destination_ip   = "${var.jumpbox_ext_ip}"
+        source_port      = "any"
+        source_ip        = "any"
+    }
+
+    rule {
+        description      = "allow-outbound"
+        policy           = "allow"
+        protocol         = "any"
+        destination_port = "any"
+        destination_ip   = "any"
+        source_port      = "any"
+        source_ip        = "${var.mgt_net_cidr}"
+    }
+}
